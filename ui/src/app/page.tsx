abc87e3e5e61455c7024d16a63bc8b03eb3aa5cd
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2023-12-10 15:24:36 +0000

    chore: update home pricing

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 import { FeaturesSection } from "@/components/Features";
 import { HowItWorksSection } from "@/components/HowItWorks";
-import { PricingPlans } from "@/components/Pricing";
 import { ProductGallery } from "@/components/ProductGallery";
 import { useRef } from "react";
 import { motion, useInView } from "framer-motion";
-import { Link } from "react-scroll";
 import SlidingBackground from "@/components/SlidingBackground";
+import { Dialog, DialogContent } from "@radix-ui/react-dialog";
+import { Button } from "@/components/ui/button";
+import { Link } from "react-scroll";
 
 export default function Home() {
   const ref = useRef(null);
@@ -14,6 +15,98 @@ export default function Home() {
 
   const ref2 = useRef(null);
   const isInView2 = useInView(ref2, { once: true });
+
+  const Tiers = () => {
+    const paymentTierOpts = [
+      {
+        name: "Basic",
+        price: 5.99,
+        features: [
+          { label: "Storage:", value: "10 designs (p/w)" },
+          { label: "Storage Limit:", value: "1GB (p/m)" },
+          { label: "AI Enhance & Upscale:", value: "5 (p/d)" },
+          { label: "Mockup Generation:", value: "2 (p/d)" },
+          { label: "Basic Support", value: "" },
+        ],
+        isPopular: false,
+      },
+      {
+        name: "Pro",
+        price: 19.99,
+        features: [
+          { label: "Storage:", value: "500 designs (p/w)" },
+          { label: "Storage Limit:", value: "5GB (p/m)" },
+          { label: "AI Enhance & Upscale:", value: "250 (p/d)" },
+          { label: "Mockup Generation:", value: "250 (p/d)" },
+          { label: "Priority Support", value: "" },
+          { label: "Product Requests", value: "" },
+          { label: "New feature Early-Access", value: "" },
+        ],
+        isPopular: true,
+      },
+      {
+        name: "Business",
+        price: 50,
+        features: [
+          { label: "Storage:", value: "2500 designs (p/w)" },
+          { label: "Storage Limit:", value: "50GB (p/m)" },
+          { label: "AI Enhance & Upscale:", value: "1250 (p/d)" },
+          { label: "Mockup Generation:", value: "1250 (p/d)" },
+          { label: "Priority Support", value: "" },
+          { label: "Product Requests", value: "" },
+          { label: "New feature Early-Access", value: "" },
+        ],
+        isPopular: false,
+      },
+    ];
+
+    return (
+      <Dialog open={true}>
+        <DialogContent className="text-accent opacity-90 bg-background rounded-md overflow-ellipsis mb-2 w-full flex-wrap">
+          <div className="grid md:grid-cols-3 justify-between mx-auto gap-4 p-4">
+            {paymentTierOpts.map((tier) => (
+              <div
+                key={tier.name}
+                className="flex flex-col justify-between p-4 rounded-md "
+              >
+                <div className="flex flex-col items-center">
+                  <h3 className="text-3xl font-bold">{tier.name}</h3>
+                  <h4 className="text-xl font-bold">${tier.price}</h4>
+                  <p className="text-sm text-center">Per Month</p>
+                </div>
+                <div className="flex flex-col items-center">
+                  {tier.isPopular && (
+                    <div className="text-xs font-bold bg-accent text-background p-1 rounded-md">
+                      Popular
+                    </div>
+                  )}
+                  <Button
+                    className="mt-4"
+                    onClick={() => {
+                      window.location.href = "/dashboard";
+                    }}
+                  >
+                    Select
+                  </Button>
+                </div>
+                <div className="flex flex-col items-center">
+                  {tier.features.map((feature) => (
+                    <div
+                      key={feature.value}
+                      className="flex justify-between w-full"
+                    >
+                      <p className="text-lg">{feature.label}</p>
+                      <p className="text-lg">{feature.value}</p>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </div>
+        </DialogContent>
+      </Dialog>
+    );
+  };
 
   return (
     <div className="mx-auto">
@@ -67,7 +160,7 @@ export default function Home() {
           animate={isInView2 ? { opacity: 1, y: 0 } : { opacity: 0, y: 20 }}
           transition={{ duration: 0.5 }}
         >
-          <PricingPlans />
+          <Tiers />
           <div ref={ref2} />
         </motion.div>
 
