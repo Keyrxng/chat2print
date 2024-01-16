5a1d3da565cd3ac66cda9ec44ca1044f8d8feaa8
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-01-16 15:35:48 +0000

    chore: v anal

--- ui/src/app/layout.tsx
+++ ui/src/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { Toaster } from "@/components/ui/toaster";
-
+import { Analytics } from "@vercel/analytics/react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -75,6 +75,7 @@ export default function RootLayout({
           <Toaster />
         </div>
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
